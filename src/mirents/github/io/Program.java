0cf949a4cb12e5b84c07621923efeafa50bcbb54
Mirents <mirent@mail.ru>
2021-01-24 16:32:02 +0300

    Now Set Project

---
+++ src/mirents/github/io/Program.java
@@ -0,0 +1,12 @@
+/*
+ * Основной класс для запуска программы
+ */
+package mirents.github.io;
+
+public class Program {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+    
+}
