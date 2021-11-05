7793a3077b4aa91f338174e14720c27d178ce6ee
Mirents <mirent@mail.ru>
2021-11-05 16:56:53 +0300

    Создан проект

---
+++ src/main/java/io/mirent/study_sql/StudySqlApplication.java
@@ -0,0 +1,13 @@
+package io.mirent.study_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mirent/study_sql/StudySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mirent.study_sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
